3f0f852da678c199ee10d8f3f96c936ef8f92f04
mk070 <madhan.king51@gmail.com>
2024-09-19 11:02:03 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
     <div className="min-h-screen bg-gradient-to-br  from-slate-50 via-gray-50 to-gray-100 relative overflow-hidden font-space-grotesk">
       {/* Decorative Blob for an Abstract Effect */}
       <div className="absolute top-0 right-0 w-72 h-72 bg-gradient-to-r from-green-400 to-blue-500 opacity-30 rounded-full filter blur-3xl z-0"></div>
-      <div className="absolute bottom-0 left-0 w-96 h-96 bg-gradient-to-l from-blue-400 to-green-500 opacity-30 rounded-full filter blur-3xl z-0"></div>
+      <div className="absolute top-96 left-0 w-96 h-96 bg-gradient-to-l from-blue-400 to-green-500 opacity-30 rounded-full filter blur-3xl z-0"></div>
 
       {/* Floating Icons */}
       <div className="absolute top-36 left-10 w-16 h-16 bg-white  p-4 rounded-full shadow-lg animate-float animation-delay-2000">
@@ -30,10 +30,10 @@ const App = () => {
       <div className="absolute top-1/4 right-20 w-16 h-16 p-4 bg-white rounded-full shadow-lg animate-float animation-delay-4000">
         <img src={icon3} alt="AI Brain Icon" />
       </div>
-      <div className="absolute bottom-96 left-48 w-16 h-16 p-4 bg-white rounded-full shadow-lg animate-float animation-delay-4000">
+      <div className="absolute top-[400px] left-48 w-16 h-16 p-4 bg-white rounded-full shadow-lg animate-float animation-delay-4000">
         <img src={icon4} alt="AI Brain Icon" />
       </div>
-      <div className="absolute bottom-32 left-20 w-16 h-16 p-4 bg-white rounded-full shadow-lg animate-float animation-delay-4000">
+      <div className="absolute top-[600px] left-20 w-16 h-16 p-4 bg-white rounded-full shadow-lg animate-float animation-delay-4000">
         <img src={icon5} alt="AI Brain Icon" />
       </div>
 
